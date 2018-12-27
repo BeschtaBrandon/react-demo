d42f3d4bed7cc06106e6e2b019da22261300cfef
BeschtaBrandon <blbeschta@wisc.edu>
2018-12-26 20:13:12 -0600

    Add Contact Page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Breadcrumb, PageHeader } from 'react-bootstrap';
 
 class Contact extends Component {
+  renderContactHeader = () => {
+    return (
+      <PageHeader>
+        Contact
+      </PageHeader>
+    );
+  }
+
+  renderContactContent = () => {
+    return (
+      <div>
+        <p className="lead">Plese feel free to reach out!</p>
+        <Breadcrumb>
+          <Breadcrumb.Item href="mailto:beschtabra2@gmail.com">Email</Breadcrumb.Item>
+          <Breadcrumb.Item href="http://facebook.com">
+          Facebook
+          </Breadcrumb.Item>
+          <Breadcrumb.Item href="https://www.linkedin.com/in/brandonbeschta/">LinkedIn</Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+    );
+  }
+
   render () {
     return (
       <div>
-        <a href="https://www.youtube.com/watch?v=QxU6jF7_YTU">Cash me outside how 'bout dat</a>
+        { this.renderContactHeader() }
+        { this.renderContactContent() }
       </div>
     );
   }
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Panel } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Panel, PageHeader } from 'react-bootstrap';
 import moment from 'moment'
 
 import './Work.scss';
 
 class Work extends Component {
 
+  renderWorkHeader = () => {
+    return (
+      <PageHeader>
+        Professional Experiences
+      </PageHeader>
+    );
+  }
+
   renderWorkContent = () => {
     const time = moment().format('MMMM Y');
 
     return (
       <ListGroup>
-        <ListGroupItem header="Paradigm" bsStyle="success" href="http://myparadigm.com">Jan 2018 - {time}</ListGroupItem>
-        <ListGroupItem header="University of Wisconsin - Madison Mechanical Engineering Department" href="http://wisc.edu">
+        <ListGroupItem header="Paradigm - Web Developer" bsStyle="success" href="http://myparadigm.com">Jan 2018 - {time}</ListGroupItem>
+        <ListGroupItem header="University of Wisconsin Madison Mechanical Engineering Department - Software Engineer" href="http://wisc.edu">
           Aug 2016 - Jan 2018
         </ListGroupItem>
-        <ListGroupItem header="Thomson Retuers" href="http://reuters.com">
+        <ListGroupItem header="Thomson Retuers - Software Engineer" href="http://reuters.com">
           Oct 2014 - Nov 2016
         </ListGroupItem>
-        <ListGroupItem header="Maverick Software Consulting" href="http://mavericksoftware.com">
+        <ListGroupItem header="Maverick Software Consulting - Software Developer Intern" href="http://mavericksoftware.com">
           April 2015 - Jun 2016
         </ListGroupItem>
       </ListGroup>
@@ -36,6 +44,7 @@ class Work extends Component {
   render () {
     return (
       <div>
+        { this.renderWorkHeader() }
         { this.renderWorkContent() }
         { this.renderEducation() }
       </div>
