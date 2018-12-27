d24f9d2ad0c6e44b9d880b6738d389faabc8ceb3
BeschtaBrandon <blbeschta@wisc.edu>
2018-12-26 20:42:51 -0600

    Add personal portrait

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import { Col, Grid, Image, Row, PageHeader, Panel } from 'react-bootstrap';
 
 import './Home.scss';
-import nasaPhoto from '../../images/outer_space.jpg'
+import nasaPhoto from '../../images/outer_space.jpg';
+import portrait from '../../images/portrait.jpg';
 
 class Home extends Component {
 
   renderAboutSection = () => {
+    const paragraph = "Currently working as web developer who is eager to learn new web technologies and develop stronger software skills. Able to communicate well with a team, and thrive in a company who encourages their employees to question the status-quo and adapt to this ever-changing world.";
+
     return (
       <div className="well">
-        Currently working as web developer who is eager to learn new web technologies and develop stronger software skills. Able to communicate well with a team, and thrive in a company who encourages their employees to question the status-quo and adapt to this ever-changing world.
+        <Grid>
+          <Row>
+            <Col xs={6} md={9}>
+              <div className="paragraph">
+                { paragraph }
+              </div>
+            </Col>
+            <Col xs={6} md={3} className="col-md-push-1">
+              <Image src={portrait} responsive />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
@@ -33,10 +47,10 @@ class Home extends Component {
              </Panel.Heading>
              <Grid>
                <Row>
-                 <Col xs={4}>
+                 <Col xs={6} md={4}>
                   <Image src={nasaPhoto} responsive />
                  </Col>
-                 <Col xs={8}>
+                 <Col xs={6} md={8}>
                   <Panel.Body>{paragraph}</Panel.Body>
                  </Col>
                </Row>
