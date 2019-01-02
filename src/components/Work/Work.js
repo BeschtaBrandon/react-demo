1fb6691aeedb81ae81d20098d936f4c9947c4cc0
BeschtaBrandon <blbeschta@wisc.edu>
2019-01-01 20:17:06 -0600

    add work images

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Panel, PageHeader } from 'react-bootstrap';
+import { Image, ListGroup, ListGroupItem, Panel, PageHeader } from 'react-bootstrap';
 import moment from 'moment'
 
+import reuters from '../../images/reuters.png';
+import uw from '../../images/uw.jpg';
+import wts from '../../images/wtsparadigm.png';
+import mav from '../../images/mavconsulting.jpg';
 import './Work.scss';
 
 class Work extends Component {
@@ -19,30 +23,42 @@ class Work extends Component {
 
     return (
       <ListGroup>
-        <ListGroupItem header="Paradigm - Web Developer" bsStyle="success" href="http://myparadigm.com">
-          Jan 2018 - {time}
-          <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;Responsible for developing web applications written using ReactJs, Laravel, PHP, and C#. Also, assist in deploying these apps to production environments for a manifold of construction
-clients. Our teams uses Docker Swarm and hosts our services on Azure.
-          </p>
+        <ListGroupItem className="media" header="Paradigm - Web Developer" bsStyle="success" href="http://myparadigm.com">
+          <div className="media-left">
+            <Image className="media-object" src={wts} />
+          </div>
+          <div class="media-body">
+            <h5 class="media-heading">Jan 2018 - {time}</h5>
+            &nbsp;&nbsp;&nbsp;&nbsp;Responsible for developing web applications written using ReactJs, Laravel, PHP, and C#. Also, assist in deploying these apps to production environments for a manifold of construction
+    clients. Our teams uses Docker Swarm and hosts our services on Azure.
+          </div>
         </ListGroupItem>
         <ListGroupItem header="University of Wisconsin Madison Mechanical Engineering Department - Software Engineer" href="http://wisc.edu">
-          Aug 2016 - Jan 2018
-          <p>
+          <div className="media-left">
+            <Image className="media-object" src={uw} />
+          </div>
+          <div className="media-body">
+            <h5 class="media-heading">Aug 2016 - Jan 2018</h5>
           &nbsp;&nbsp;&nbsp;&nbsp;Implement and design a web application to assist family caregivers of people with dementia or Alzheimer’s disease. This app helps enable informal caregivers, who are often disconnected from each other, to better communicate and document information related to the person with dementia. Additionally, as caregivers document a patient’s dementia-related behaviors over time, the platform will use machine learning to generate caregiver strategies that are tailored for the specific patient.
-          </p>
+          </div>
         </ListGroupItem>
         <ListGroupItem header="Thomson Reuters - Software Engineer" href="http://reuters.com">
-          Oct 2014 - Nov 2016
-          <p>
+        <div className="media-left">
+          <Image className="media-object" src={reuters} />
+        </div>
+        <div className="media-body">
+          <h5 class="media-heading">Jun 2016 - Sep 2017</h5>
             &nbsp;&nbsp;&nbsp;&nbsp;Built a database migration tool using Java, Spring Boot, and Flyway to keep database schemas up to date. Implemented analytics tools to provide statistics on server usage and cluster management. In addition, wrote automated tests using Selenium framework to improve the quality of our analytics tools.
-          </p>
+          </div>
         </ListGroupItem>
         <ListGroupItem header="Maverick Software Consulting - Software Developer Intern" href="http://mavericksoftware.com">
-          April 2015 - Jun 2016
-          <p>
+        <div className="media-left">
+          <Image className="media-object" src={mav} />
+        </div>
+        <div className="media-body">
+          <h5 class="media-heading">April 2015 - Jun 2016</h5>
             &nbsp;&nbsp;&nbsp;&nbsp;Assisted developers at Thomson Reuters write pages for a web application that help lawyers and legal representatives organize their case information. Some technologies included in this process were Java, Spring Boot and Selenium.
-          </p>
+          </div>
         </ListGroupItem>
       </ListGroup>
     );
